3fe91a091d4ab7fc2cf8ba85feabbd3542fab30f
zakariya gujjar <168914532+zakariya617@users.noreply.github.com>
2024-05-25 16:59:08 +0500

    changes

--- src/components/brands.js
+++ src/components/brands.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 
 const Brands = ({ Image }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <Box sx={{ height: '100%', width: '70%' }}>
-      <img src={Image} alt="Logo" style={{ height: '90%', width: '110%', }} />
+      <img 
+        src={Image} 
+        alt="Logo" 
+        style={{ 
+          height: '90%', 
+          width: '100%', 
+          transform: isHovered ? 'scale(1.1)' : 'scale(1)',
+          transition: 'transform 0.3s ease-in-out'
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      />
     </Box>
   );
 };
--- src/pages/navebar.js
+++ src/pages/navebar.js
@@ -9,10 +9,10 @@ const NaveBar = () => {
           <img
             src='https://cdn.shopify.com/s/files/1/0143/1552/0054/files/Bata-logo_1_493d53de-402c-4029-9c9f-09562279dd99.png?v=1694065416'
             alt="Logo"
-            style={{ height: '40%', width: '120%' }}
+            style={{ height: '50%', width: '97%' }}
           />
         </Box>
-        <Typography sx={{ fontSize: '18px', fontWeight: 'bold', color: 'black', cursor: 'pointer', marginRight: '2%' }} >SNEAKERS</Typography>
+       
         <Typography sx={{ fontSize: '18px', fontWeight: 'bold', color: 'black', cursor: 'pointer', marginRight: '2%' }}>MEN</Typography>
         <Typography sx={{ fontSize: '18px', fontWeight: 'bold', color: 'black', cursor: 'pointer', marginRight: '2%' }} >WOMEN</Typography>
         <Typography sx={{ fontSize: '18px', fontWeight: 'bold', color: 'black', cursor: 'pointer', marginRight: '2%' }} >KIDS</Typography>
@@ -20,15 +20,16 @@ const NaveBar = () => {
         <Typography sx={{ fontSize: '18px', fontWeight: 'bold', color: 'black', cursor: 'pointer', marginRight: '2%' }} >BAGS</Typography>
         <Typography sx={{ fontSize: '18px', fontWeight: 'bold', color: 'black', cursor: 'pointer', marginRight: '2%' }} >BATACLUB</Typography>
         <Typography sx={{ fontSize: '18px', fontWeight: 'bold', color: 'black', cursor: 'pointer', marginRight: '2%' }} >SALE</Typography>
-        {/* Search bar */}
+        
         <TextField
           id="search"
-          label="Search"
-          variant="outlined"
+          label="Search...."
+          variant="standard"
           size="large"
-          sx={{ ml: 2 ,width:'95%',marginRight:'2%'}}
+          sx={{ ml: 0,width:'95%',marginRight:'2%',borderColor:'blue'}}
         />
       </Box>
+      
 
     </Box>
   );
